30e8132e9937695fd7d37819cc958db5046652cf
Timo <Schwimo@users.noreply.github.com>
2021-05-20 11:25:06 +0200

    Update setup_ros.bash

--- scripts/setup_ros.bash
+++ scripts/setup_ros.bash
@@ -26,9 +26,11 @@ installRosMelodic() {
 }
 
 installRosNoetic() {
+    sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+    sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
     sudo apt-get install -y ros-noetic-desktop
     sudo apt-get install -y ros-noetic-rosbridge-server   
-    sudo apt-get install -y ros-noetic-plotjuggler 
+    sudo apt-get install -y ros-noetic-plotjuggler ros-noetic-plotjuggler-ros    
     sudo apt-get install -y python3-pip python3-yaml
     sudo apt-get install -y python3-catkin-tools
     sudo apt-get install -y python3-rosdep
